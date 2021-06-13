4737f694e5bdeec10c2942ce3227c7a81d5cb87d
joshtheprogrammer <joshuahonguyen@gmail.com>
2021-06-13 17:12:41 -0500

    random time if no time defined

--- src/popup.js
+++ src/popup.js
@@ -1,12 +1,25 @@
 // Initialize
 let TIMEVAL = document.getElementById('TIMEVAL');
 
+TIMEVAL.placeholder = Math.floor(Math.random() * 43200);
+
+TIMEVAL.addEventListener("click" , async () => {
+  chrome.storage.sync.get("TIME", ({ TIME }) => {
+    TIMEVAL.value = TIME;
+  });
+});
+
 //button is clicked
 setTime.addEventListener("click", async () => {
   let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
   
-  let TIME = document.getElementById('TIMEVAL').value;
-  
+  if (parseInt(document.getElementById('TIMEVAL').value)) {
+    var TIME = document.getElementById('TIMEVAL').value;
+  }
+  else {
+    var TIME = document.getElementById('TIMEVAL').placeholder;
+  }
+
   chrome.storage.sync.set({TIME}, function () { 
     chrome.scripting.executeScript({
       target: { tabId: tab.id },
@@ -17,17 +30,9 @@ setTime.addEventListener("click", async () => {
 
 // script
 function setVideoTime() {
-  try {
-    chrome.storage.sync.get("TIME", ({ TIME }) => {
-      if (window.location.href.includes("youtube.com/watch")) {
-        window.location.href = window.location.href.split("&t=")[0] + `&t=${TIME}`;
-      }
-    });
-  }
-  catch {
+  chrome.storage.sync.get("TIME", ({ TIME }) => {
     if (window.location.href.includes("youtube.com/watch")) {
-      alert("0");
-      window.location.href = window.location.href.split("&t=")[0] + `&t=0`;
+        window.location.href = window.location.href.split("&t=")[0] + `&t=${TIME}`;
     }
-  }
+  });
 }
